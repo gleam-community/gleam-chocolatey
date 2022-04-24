50f0b467a0b4c2a587b6bc5ed47f7989a8c62b15
HarryET <29015545+HarryET@users.noreply.github.com>
2022-04-24 20:28:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# Chocolatey Gleam
+![Chocolatey Version](https://img.shields.io/chocolatey/v/gleam)
+Allows windows users to install the Gleam programing language through Chocolatey
+
+## ⚠️ Note
+This is unofficial and not maintained by the Gleam team, it is managed and maintained by [HarryET](https://github.com/harryet)
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/gleam-lang/gleam/releases/download/v0.21.0/gleam-v0.21.0-windows-64bit.zip'
+$urlChecksum = 'https://github.com/gleam-lang/gleam/releases/download/v0.21.0/gleam-v0.21.0-windows-64bit.zip.sha512'
+
+Get-WebFile `
+  -Url $urlChecksum `
+  -FileName "$toolsDir/v0.21.0-checksum.sha512" `
+  -Quiet
+
+$checksum = Get-Content "$toolsDir/v0.21.0-checksum.sha512" -Raw
+$checksum = $checksum.Split("*")[0]
+$checksum = $checksum.Trim()
+
+Install-ChocolateyZipPackage `
+  -PackageName "Gleam" `
+  -Url64Bit $url64 `
+  -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
+  -Checksum64 $checksum `
+  -ChecksumType64 "sha512" `
