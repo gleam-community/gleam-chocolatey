b35ad1963350d22b465eeeb642e99e58440524df
HarryET <HarryET@users.noreply.github.com>
2023-02-07 09:30:35 +0000

    feat(auto): Release 0.26.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
-$url64      = 'https://github.com/gleam-lang/gleam/releases/download/v0.26.2/gleam-v0.26.2-x86_64-pc-windows-msvc.zip'
+$url64      = 'https://github.com/gleam-lang/gleam/releases/download/0.26.2/gleam-0.26.2-x86_64-pc-windows-msvc.zip'
 
-$checksum = "9cb57bed019b6be5c3957ee8640f62eb795a0df98ce44e92adb685db4cc8d5f072d774b992dc0514ca5058221007026e7321f349447503011c7bab6d5fc82a94"
+$checksum = "Not Found"
 
 Install-ChocolateyZipPackage `
   -PackageName "Gleam" `
