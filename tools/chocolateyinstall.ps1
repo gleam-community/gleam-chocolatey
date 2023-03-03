25911a6a92eb6ae7967bbefd98e27d62c8785a6d
Kayla Washburn <mckayla@hey.com>
2023-03-02 19:24:36 -0700

    Gleam v0.27.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
-$ErrorActionPreference = 'Stop'; # stop on all errors
-$url64      = 'https://github.com/gleam-lang/gleam/releases/download/0.26.2/gleam-0.26.2-x86_64-pc-windows-msvc.zip'
-
-$checksum = "Not Found"
+$ErrorActionPreference = "Stop"; # stop on all errors
+$url64    = "https://github.com/gleam-lang/gleam/releases/download/v0.27.0/gleam-v0.27.0-x86_64-pc-windows-msvc.zip"
+$checksum = "2efdef80f649aa88c9639fa416bd2d97bdfc029375fad34f3ed9468adf858f939c5c7f881e387f48ca110da89b3fd208eeec1f5b35172806610f62d50065ae72"
 
 Install-ChocolateyZipPackage `
   -PackageName "Gleam" `
