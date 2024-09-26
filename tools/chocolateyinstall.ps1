3e3cae029cfbcd043d5fa446c18815184edb75f8
McKayla Washburn <mckayla@hey.com>
2024-09-26 10:28:16 -0600

    v1.5.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"; # stop on all errors
-$url64    = "https://github.com/gleam-lang/gleam/releases/download/v1.5.0/gleam-v1.5.0-x86_64-pc-windows-msvc.zip"
-# Taken from https://github.com/gleam-lang/gleam/releases/download/v1.5.0/gleam-v1.5.0-x86_64-pc-windows-msvc.zip.sha512
-$checksum = "c79b976e9686a0d98274d7e5731c14b207bb87d627774f7cae77a48ec080cc9f7179f5c5e1a6ef16f0e3cc8382d524fd44de3541ee7b27f561c9f35b4a3a05dd"
+$url64    = "https://github.com/gleam-lang/gleam/releases/download/v1.5.1/gleam-v1.5.1-x86_64-pc-windows-msvc.zip"
+# Taken from https://github.com/gleam-lang/gleam/releases/download/v1.5.1/gleam-v1.5.1-x86_64-pc-windows-msvc.zip.sha512
+$checksum = "f16d286c74ea3a01ada1da0a0b46717bada31d8b0b26e4c9b282744ecdde819e861c0af8ce31dac5637bca7d2d02084a5a37838cd93f97b7abe03374fb6a7b5a"
 
 Install-ChocolateyZipPackage `
   -PackageName "Gleam" `
